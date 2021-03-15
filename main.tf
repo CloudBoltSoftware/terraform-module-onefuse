2a6025d57fa8ca453f6b32966048029fc3acfbe8
Sid Smith <ssmith@sovlabs.com>
2021-03-15 19:44:36 -0400

    Added ability to dynaicall call OneFuse modules from main OneFuse module

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ module "ad_computer" {
     source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ad"
     count = "${var.ad_policy == "" ? 0 : var.instance_count}"
     policy = var.ad_policy
-    hostname = "${module.name.hostname == "" ? var.hostname : module.name[count.index].hostname}"
+    hostname = "${module.name[count.index].hostname == "" ? var.hostname : module.name[count.index].hostname}"
     template_properties = var.template_properties
 }
 
 module "ipam" {
     source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ipam"
     policy = var.ipam_policy
-    hostname = "${module.name.hostname == "" ? var.hostname : module.name[count.index].hostname}"
+    hostname = "${module.name[count.index].hostname == "" ? var.hostname : module.name[count.index].hostname}"
     template_properties = var.template_properties
 }
 
@@ -25,6 +25,6 @@ module "dns" {
     policy = var.dns_policy
     ip_address = module.ipam.ip_address
     dns_zones = module.name.dns_suffix
-    hostname = "${module.name.hostname == "" ? var.hostname : module.name[count.index].hostname}"
+    hostname = "${module.name[count.index].hostname == "" ? var.hostname : module.name[count.index].hostname}"
     template_properties = var.template_properties
 }
