00bcbcd91671c4b54091bb867578402303df104e
Sid Smith <ssmith@sovlabs.com>
2021-03-15 18:19:08 -0400

    Added ability to dynaicall call OneFuse modules from main OneFuse module

--- variables.tf
+++ variables.tf
@@ -1,19 +1,24 @@
 variable "name_policy" {
   type = string
+  default = ""
 }
 
 variable "ad_policy" {
   type = string
+  default = ""
 }
 
 variable "ipam_policy" {
   type = string
+  default = ""
 }
 
 variable "dns_policy" {
   type = string
+  default = ""
 }
 
 variable "template_properties" {
   type = map
+  default = ""
 }
