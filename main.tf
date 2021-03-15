572408d773be3a799673ce88ff3f92a8f48c0fcb
Sid Smith <ssmith@sovlabs.com>
2021-03-15 19:06:10 -0400

    Added ability to dynaicall call OneFuse modules from main OneFuse module

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 module "name" {
-    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//naming?ref=v1.2-beta.1"
+    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//naming"
     policy = var.name_policy
     template_properties = var.template_properties
 }
 
 module "ad_computer" {
-    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ad?ref=v1.2-beta.1"
+    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ad"
     policy = var.ad_policy
     hostname = module.name.hostname
     template_properties = var.template_properties
 }
 
 module "ipam" {
-    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ipam?ref=v1.2-beta.1"
+    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ipam"
     policy = var.ipam_policy
     hostname = module.name.hostname
     template_properties = var.template_properties
 }
 
 module "dns" {
-    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//dns?ref=v1.2-beta.1"
+    source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//dns"
     policy = var.dns_policy
     ip_address = module.ipam.ip_address
     dns_zones = module.name.dns_suffix
