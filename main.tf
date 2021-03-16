bc23f374e802a909f194bb0b5ec82c2f71821679
Sid Smith <ssmith@sovlabs.com>
2021-03-15 20:13:12 -0400

    Added ability to dynaicall call OneFuse modules from main OneFuse module

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "ad_computer" {
     source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ad"
     count = "${var.ad_policy == "" ? 0 : var.instance_count}"
     policy = var.ad_policy
-    hostname = "${module.name[count.index].hostname == "" ? var.hostname : module.name[count.index].hostname}"
+    hostname = "${var.name_policy == "" ? var.hostname : module.name[count.index].hostname}"
     template_properties = var.template_properties
 }
 
@@ -17,7 +17,7 @@ module "ipam" {
     source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//ipam"
     count = "${var.ipam_policy == "" ? 0 : var.instance_count}"
     policy = var.ipam_policy
-    hostname = "${module.name[count.index].hostname == "" ? var.hostname : module.name[count.index].hostname}"
+    hostname = "${var.name_policy == "" ? var.hostname : module.name[count.index].hostname}"
     template_properties = var.template_properties
 }
 
@@ -25,8 +25,8 @@ module "dns" {
     source = "git::https://github.com/CloudBoltSoftware/terraform-module-onefuse.git//dns"
     count = "${var.dns_policy == "" ? 0 : var.instance_count}"
     policy = var.dns_policy
-    ip_address = "${module.ipam[count.index].ip_address == "" ? var.hostname : module.ipam[count.index].ip_address}"
+    ip_address = "${var.ipam_policy == "" ? var.hostname : module.ipam[count.index].ip_address}"
     dns_zones = module.name[count.index].dns_suffix
-    hostname = "${module.name[count.index].hostname == "" ? var.hostname : module.name[count.index].hostname}"
+    hostname = "${var.name_policy == "" ? var.hostname : module.name[count.index].hostname}"
     template_properties = var.template_properties
 }
