7523ff7164e1c3b95a31078bf2f73f01d9925d9e
Sid Smith <ssmith@sovlabs.com>
2021-03-15 20:50:47 -0400

    Added ability to dynaicall call OneFuse modules from main OneFuse module

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,43 @@
-output "info" {
-  value = merge([module.name.*.hostname], 
-                [module.name.*.dns_suffix], 
-                [module.ipam.*.ip_address], 
-                [module.ipam.*.netmask],
-                [module.ipam.*.gateway],
-                [module.ipam.*.network],
-                [module.ipam.*.subnet],
-                [module.ipam.*.primary_dns],
-                [module.ipam.*.secondary_dns],
-                [module.name.*.fqdn],
-                [module.ad_computer.*.ad_ou]
-          )
+output "hostname" {
+  value = [module.name.*.hostname]
+}
+
+output "dns_suffix" {
+  value = [module.name.*.dns_suffix]
+}
+
+output "ip_address" {
+  value = [module.ipam.*.ip_address]
+}
+
+output "netmask" {
+  value = [module.ipam.*.netmask]
+}
+
+output "gateway" {
+  value = [module.ipam.*.gateway]
+}
+
+output "network" {
+  value = [module.ipam.*.network]
+}
+
+output "subnet" {
+  value = [module.ipam.*.subnet]
+}
+
+output "primary_dns" {
+  value = [module.ipam.*.primary_dns]
+}
+
+output "secondary_dns" {
+  value = [module.ipam.*.secondary_dns]
+}
+
+output "fqdn" {
+  value =[module.name.*.fqdn]
+}
+
+output "ad_ou" {
+  value = [module.ad_computer.*.ad_ou]
 }
