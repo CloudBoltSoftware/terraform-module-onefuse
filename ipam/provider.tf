b38c0d69884f0ca9ba1eda9235fcf376bab970ea
sidsovlabs <66967367+sidsovlabs@users.noreply.github.com>
2021-08-27 07:22:03 -0400

    Update provider.tf

--- ipam/provider.tf
+++ ipam/provider.tf
@@ -2,7 +2,7 @@
   required_providers {
     onefuse = {
       source  = "CloudBoltSoftware/onefuse"
-      version = ">= 1.30.0"
+      version = ">= 1.40.0"
    }
   }
   required_version = ">= 0.13"
