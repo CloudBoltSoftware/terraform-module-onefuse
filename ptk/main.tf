ec40422eae6091a7b17a16958026618aadc16c79
sidsovlabs <66967367+sidsovlabs@users.noreply.github.com>
2021-08-27 10:07:07 -0400

    Update main.tf

--- ptk/main.tf
+++ ptk/main.tf
@@ -4,6 +4,6 @@ data "onefuse_property_set" "property_set" {
 
 
 data "onefuse_rendered_template" "properties" {
-    template = data.onefuse_static_property_set.property_set.raw
+    template = data.onefuse_property_set.property_set.raw
     template_properties = var.template_properties
 }
